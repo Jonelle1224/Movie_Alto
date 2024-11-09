ad6018d4b87dcc6c21d404e254b3236d18398656
JoeTorrs <jmarktorrecampo1224@gmail.com>
2024-11-09 23:29:36 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -46,8 +46,8 @@ if (isset($_POST['LogIn'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portal</title>
-    <link rel="stylesheet" href="../front/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../front/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../front/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../front/css/bootstrap.css">
 
 </head>
 
